43e1897b740e24149cfc29fc02dd21e6dddb240f
vinaapatricia <vinaapatricia@gmail.com>
2023-10-12 23:14:50 +0700

    first commit

---
+++ Backend_chatbot_flutter/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/bot", methods=["POST"])  # Change "method" to "methods"
+def response():
+    query = dict(request.form)["query"]
+    result = query + " " + time.ctime()
+    return jsonify({"response": result})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
